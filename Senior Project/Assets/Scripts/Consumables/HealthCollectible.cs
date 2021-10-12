c2af0d5e63afb4ba8519d8d271de3de7a91b2225
jonathan arsola <jonathan.arsola01@utrgv.edu>
2021-10-12 15:00:36 -0500

    Added more health control so food will disappear when touched.
    Made a Health Control game object. The children items will disappear when touched. Saved in the prefab folder in the consumables folder.

--- Senior Project/Assets/Scripts/Consumables/HealthCollectible.cs
+++ Senior Project/Assets/Scripts/Consumables/HealthCollectible.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
+    //These variables will keep track if the item should be shown. 
+    public bool isGone {get{return gone;}}
+    bool gone;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gone = false;
     }
 
     // Update is called once per frame
@@ -18,19 +21,27 @@ public class HealthCollectible : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        PlayerController controller = other.GetComponent<PlayerController>();
-
-        if (controller != null)
+        if(gone == false)
         {
-            Debug.Log("Symbolic health increase!");
-            /*
-            //Make these inside of the PlayerController script. 
-            if(controller.health  < controller.maxHealth)
+            PlayerController controller = other.GetComponent<PlayerController>();
+            if (controller != null)
             {
-                //Make this too. ChangeHealth function. 
-                controller.ChangeHealth(1);
-            }
-            */
-        } 
+                Debug.Log("Symbolic health increase!");
+                /*
+                //Make these inside of the PlayerController script. 
+                if(controller.health  < controller.maxHealth)
+                {
+                    //Make this too. ChangeHealth function. 
+                    controller.ChangeHealth(1);
+                    gone = true;
+                }
+                */
+            } 
+        }
+    }
+
+    public void ResetGone()
+    {
+        gone = false;
     }
 }
