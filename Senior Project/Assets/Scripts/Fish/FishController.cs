96a2556b90faa0c3d6704e04ed3a8c1cd5ed635f
jonathan arsola <jonathan.arsola01@utrgv.edu>
2021-10-15 03:57:04 -0500

    Added animation to the fish.
    Moves as if swimming and goes up and down as if moving in water.

--- Senior Project/Assets/Scripts/Fish/FishController.cs
+++ Senior Project/Assets/Scripts/Fish/FishController.cs
@@ -6,6 +6,8 @@ public class FishController : MonoBehaviour
 {
     float speed = 3.0f;
     float distanceAway = 20.0f;
+    float _amplitude = 0.5f;
+    float _frequency = 2.0f;
     public PlayerController player;
     Rigidbody controller;
     // Start is called before the first frame update
@@ -30,6 +32,7 @@ public class FishController : MonoBehaviour
     {
         Vector2 position = transform.position;
         position.x = position.x + speed * Time.deltaTime;
+        position.y = Mathf.Sin(Time.time * _frequency) * _amplitude;
         transform.position = position;
     }
 }
