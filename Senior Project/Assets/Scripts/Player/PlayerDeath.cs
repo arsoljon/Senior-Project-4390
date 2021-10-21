2bc332d3bcf4a90e7b50caac37dece31242cbe5d
jonathan arsola <jonathan.arsola01@utrgv.edu>
2021-10-21 15:12:40 -0500

    Added death animation

--- Senior Project/Assets/Scripts/Player/PlayerDeath.cs
+++ Senior Project/Assets/Scripts/Player/PlayerDeath.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
+    float deathTimer = 0f;
+    float maxDeathTimer = 3f;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("health: " + gameObject.GetComponent<PlayerController>().health);
+        SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(gameObject.GetComponent<PlayerController>().health <= 0){
-            Destroy(gameObject);
-            LevelManager.instance.Respawn();
+            deathTimer += Time.deltaTime;
+            gameObject.GetComponent<PlayerController>().isDead = true;
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1f,0f,0f, 0.7f);
+            gameObject.GetComponent<Animator>().Play("Player_Death");
+            gameObject.AddForce(Vector3.Down * 3);
+            if(deathTimer > maxDeathTimer)
+            {
+                Destroy(gameObject);
+
+                LevelManager.instance.Respawn();
+            }
         }
     }
 }
