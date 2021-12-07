ec0efbaf7a22af163dcb1132b98ceca5709a5890
Laura <70113894+lauralopez01@users.noreply.github.com>
2021-12-07 13:20:07 -0600

    bug fixes
    bug fixes

--- Senior Project/Assets/Scripts/Inventory/Pickup.cs
+++ Senior Project/Assets/Scripts/Inventory/Pickup.cs
@@ -6,27 +6,25 @@ public class Pickup : MonoBehaviour
 {
     private Inventory inventory;
     public GameObject itemButton;
-    //public GameObject effect;
     
     private void Start()
     {
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
-    
+    //When player collides with item
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            // spawn the sun button at the first available inventory slot ! 
-            
-            
+            //check whether inventory is full of items or not
             for (int i = 0; i < inventory.items.Length; i++)
             {
                 if (inventory.items[i] == 0)
-                { // check whether the slot is EMPTY
-                    //Instantiate(effect, transform.position, Quaternion.identity);
-                    inventory.items[i] = 1; // makes sure that the slot is now considered FULL
-                    Instantiate(itemButton, inventory.slots[i].transform, false); // spawn the button so that the player can interact with it
+                {
+                    // makes sure that the slot is now considered FULL
+                    inventory.items[i] = 1; 
+                    // spawn the button so that the player can interact with it/bring to existance
+                    Instantiate(itemButton, inventory.slots[i].transform, false); 
                     Destroy(gameObject);
                     break;
                 }
